034cf4a52c3e1138e95ea24ec4d010ad2cfea39c
Michiel <michiel@bitsoflove.be>
2016-10-17 15:20:20 +0200

    remove variabele

--- resources/config/variables.php
+++ resources/config/variables.php
@@ -21,7 +21,6 @@ return [
     'white'                  => '#ffffff',
 
     // Branding
-    'brand-primary'          => '#61259e',
     'brand-success'          => '#24ce7b',
     'brand-info'             => '#38b5e6',
     'brand-warning'          => '#f69630',
